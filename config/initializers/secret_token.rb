c91fab24bd618477bdcc9aece663f68d07f281da
Ryan Brunner <ryan@ryanbrunner.com>
2012-05-07 19:07:32 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoRoboRails::Application.config.secret_token = '901555149b0fef5598cdd7f9dce8475e24aaf316c045ada38a081eab710c47b225e4d65249776009a333b400b1f83d67b8f1272f1be2db9a56189ceda61f5473'
